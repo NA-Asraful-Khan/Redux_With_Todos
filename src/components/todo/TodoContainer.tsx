621670248d6e586021803147043c67074bdca82a
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-12-05 18:58:26 +0600

    Integrate Live API

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -21,28 +21,54 @@ const TodoContainer = () => {
   //* From Server Store
   const { data: todos, isLoading } = useGetTodosQuery(priority);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
   return (
     <div>
       <div className="flex justify-between mb-5">
         <AddTodoModal />
         <TodoFilter priority={priority} setPriority={setPriority} />
       </div>
-      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] ">
-        {todos?.data?.length > 0 ? (
-          <div className="bg-white p-5 w-full h-full space-y-3 rounded-lg">
-            {todos?.data?.map((item: Todo, i: number) => (
-              <TodoCard key={i} {...item} />
-            ))}
-          </div>
-        ) : (
-          <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
-            <p>There is no task pending</p>
-          </div>
-        )}
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center">
+          <svg
+            className="w-[50%] "
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 300 150"
+          >
+            <path
+              fill="none"
+              stroke="#FF156D"
+              stroke-width="8"
+              stroke-linecap="round"
+              stroke-dasharray="300 385"
+              stroke-dashoffset="0"
+              d="M275 75c0 31-27 50-50 50-58 0-92-100-150-100-28 0-50 22-50 50s23 50 50 50c58 0 92-100 150-100 24 0 50 19 50 50Z"
+            >
+              <animate
+                attributeName="stroke-dashoffset"
+                calcMode="spline"
+                dur="2"
+                values="685;-685"
+                keySplines="0 0 1 1"
+                repeatCount="indefinite"
+              ></animate>
+            </path>
+          </svg>
+        </div>
+      ) : (
+        <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] ">
+          {todos?.data?.length > 0 ? (
+            <div className="bg-white p-5 w-full h-full space-y-3 rounded-lg">
+              {todos?.data?.map((item: Todo, i: number) => (
+                <TodoCard key={i} {...item} />
+              ))}
+            </div>
+          ) : (
+            <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
+              <p>There is no task pending</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "http://todoapp.n-a-asraful-khan.xyz/",
   plugins: [react()],
   resolve: {
     alias: {
